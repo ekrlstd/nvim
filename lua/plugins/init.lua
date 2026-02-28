1e35dbd1398c5e4c95694b979f69aa50404046e4
ekrlstd <ebbekarlstad@proton.me>
2026-02-28 12:00:04 +0100

    Backup: 2026-02-28_12:00

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -18,24 +18,7 @@ return {
   },
   {
     "sphamba/smear-cursor.nvim",
-    opts = {
-      max_length = 2,
-      max_length_insert_mode = 1,
-      vertical_bar_cursor = false, -- or true, matching your actual cursor shape
-      -- Smear cursor when switching buffers or windows.
-      smear_between_buffers = false,
-      -- Smear cursor when moving within line or to neighbor lines.
-      -- Use `min_horizontal_distance_smear` and `min_vertical_distance_smear` for finer control
-      smear_between_neighbor_lines = true,
-      -- Draw the smear in buffer space instead of screen space when scrolling
-      scroll_buffer_space = true,
-      -- Set to `true` if your font supports legacy computing symbols (block unicode symbols).
-      -- Smears and particles will look a lot less blocky.
-      legacy_computing_symbols_support = false,
-      -- Smear cursor in insert mode.
-      -- See also `vertical_bar_cursor_insert_mode` and `distance_stop_animating_vertical_bar`.
-      smear_insert_mode = true,
-    },
+    opts = {},
   },
   {
     "3rd/image.nvim",
